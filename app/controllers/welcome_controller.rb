2b49e6453a3ba678b4b0941b4295beed4f507bce
rgarcia <voodoorai2000@gmail.com>
2013-08-21 21:17:27 +0200

    basic rails app with sms

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  
+  def index
+    sms_fu = SMSFu::Client.configure(:delivery => :action_mailer)
+#    sms_fu = SMSFu::Client.configure(:delivery => :pony, :pony_config => { :via => :sendmail })
+    sms_fu.deliver(ENV['TEST_PHONE'],"vodafone-spain","message")
+  end
+end
