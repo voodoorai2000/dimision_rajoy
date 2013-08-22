487d0863a97f547ad47b5c03655bc7840968a2c9
rgarcia <voodoorai2000@gmail.com>
2013-08-23 00:38:58 +0200

    share in social networks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def share_text
+    "Plebiscito para la dimision de Mariano Rajoy de la Presidencia DimisionRajoy.com"
+  end
+  
+  def display_in_popup
+    {onclick: "return !window.open(this.href, 'Twitter', 'width=500,height=500')", target: "_blank"}
+  end
+  
 end
