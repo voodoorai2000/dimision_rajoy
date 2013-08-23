1fc6957b29381263edf9e45fe661fd3a3c41edb2
rgarcia <voodoorai2000@gmail.com>
2013-08-23 05:45:58 +0200

    allow city in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :city)
   end
 end
