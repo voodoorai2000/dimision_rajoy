da719fd33174d4bc6e79f65042ea809607061358
rgarcia <voodoorai2000@gmail.com>
2013-08-23 05:17:51 +0200

    home

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to root_url
+      redirect_to "/#comments_header", notice: "Su opinión ha sido guardada"
     else
       render :edit
     end
